86e11d3624efbac238904fbf09bfdef61dcb8fed
Sudiip <115084777+piidus@users.noreply.github.com>
2024-09-10 14:20:01 +0530

    update

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flet import Page, app
 from controllers.page_controller import route_change
+import warnings
+warnings.filterwarnings(action='ignore', category=DeprecationWarning)
 
 def main(page: Page):
     page.title = "InChat"
-    page.window.width = 300
+    # page.window.width = 300
     page.on_route_change = route_change
 
     # Start on LoginPage or another default page
--- pages/login_setup/login_page.py
+++ pages/login_setup/login_page.py
@@ -1,4 +1,6 @@
-from flet import Page, Container, Card, Column, Text
+from flet import (Page, Container, Card, Column, Text, border, border_radius, 
+                  colors, alignment, CrossAxisAlignment, MainAxisAlignment)
+
 class LoginPage:
     def __init__(self, page: Page, **kwargs):
         self.page = page
@@ -6,6 +8,37 @@ class LoginPage:
         self.some_value = kwargs.get("some_value", "Default Value")
         self.ip_address = kwargs.get("ip_address", "No IP Address Provided")
     
+    def size(self, height:int = 100, width:int= 100)->int:
+        new_height = self.page.window.height * (height /100)
+        new_wight = self.page.window.width * width /100
+        # print(self.page.window.width, new_wight)
+        return new_height, new_wight
 
+    def main_card(self):
+        card_height, card_width = self.size(50, 80)
+        # print(card_height, card_width)
+        card= Card(content=Text(value='New'),   
+                    # align = 'center',
+                   height=card_height,
+                   width=card_width,
+                   elevation=10, shadow_color='5555')
+        list_view = Column(
+            alignment=MainAxisAlignment.CENTER,
+            # horizontal_alignment=CrossAxisAlignment.CENTER,
+            controls=[card,],
+            expand=True)
+        
+        # def build(self):
+        return list_view
     def build(self):
-        return Container(content=Column(controls=[Text(value="Hi")]))
+        # _, width = self.size(width=80)
+        return Container(
+            alignment=alignment.center,
+            bgcolor=colors.BLUE_GREY_100,
+            border=border.all(2, colors.BLACK),
+            border_radius= border_radius.all(10),
+            padding= 30,
+            margin=30,
+            expand_loose=True,
+            expand=True,
+            content=Column(controls=[ self.main_card()]))
