490d75cce24e07c84fd92fd22dcd18cdb0050bb4
Sudiip <115084777+piidus@users.noreply.github.com>
2024-09-10 17:33:42 +0530

    height adjust

--- pages/login_setup/login_page.py
+++ pages/login_setup/login_page.py
@@ -7,24 +7,33 @@ class LoginPage:
         self.page.bgcolor = "#e6f7ff"
         self.some_value = kwargs.get("some_value", "Default Value")
         self.ip_address = kwargs.get("ip_address", "No IP Address Provided")
-    
+        # Attempt to get the 'windowheight' value from kwargs or default to 400
+        self._page_height = float(self.page._Control__attrs.get('height', ('650', False))[0] or '600')
+        self._page_width = float(self.page._Control__attrs.get('width', ('400', False))[0] or '400')
+    def did_mount(self):
+        print('[LoginPage] __init__ called', self._page_height)
+        # self.page_height, self.page_width = 800,500
+        print('[windowheight]',self.page._Control__attrs['windowheight'])
+        print('[windowwidth]',self.page._Control__attrs['windowwidth'])
+        print('[height]',self.page._Control__attrs['height'])
+        print('[width]',self.page._Control__attrs['width'])
+        # print('[LoginPage] did_mount called', self.page_height, self.page_width)
     def size(self, height:int = 100, width:int= 100)->int:
-        new_height = self.page.window.height * (height /100)
-        new_wight = self.page.window.width * width /100
-        # print(self.page.window.width, new_wight)
+        new_height = float(self._page_height * (height /100))
+        new_wight = self._page_width * width /100
         return new_height, new_wight
 
     def main_card(self):
         card_height, card_width = self.size(50, 80)
-        # print(card_height, card_width)
-        card= Card(content=Text(value='New'),   
-                    # align = 'center',
-                   height=card_height,
-                   width=card_width,
-                   elevation=10, shadow_color='5555')
+        # print(type(card_height), card_width)
+        card= Card(content=Text(value='New'), 
+                   height=card_height,  
+                   width=card_width,                    
+                   elevation=10, 
+                   shadow_color='5555')
         list_view = Column(
             alignment=MainAxisAlignment.CENTER,
-            # horizontal_alignment=CrossAxisAlignment.CENTER,
+            horizontal_alignment=CrossAxisAlignment.CENTER,
             controls=[card,],
             expand=True)
         
